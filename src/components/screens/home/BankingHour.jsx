051fb99e4ac0a33ddc1761169bf6808da99aa7bd
valeri <valerisergeev98@gmail.com>
2023-10-13 23:18:35 -0300

    Updates

--- src/components/screens/home/BankingHour.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const BankingHour = ({sumOfDifferences, limit}) => {
-  return (
-    <div>BankingHours: {sumOfDifferences - limit}</div>
-  )
-}
-
-export default BankingHour
